d7eacea68f2998733c9ea3fc0d75b213446300a1
Beatrice Guerra <beatrice.guerra@zextras.com>
2022-04-26 12:14:14 +0200

    test: add test for drop of folders
    refs: FILES-219

--- views/components/UploadList.test.tsx
+++ views/components/UploadList.test.tsx
@@ -6,7 +6,15 @@
 
 import React from 'react';
 
-import { fireEvent, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import {
+	act,
+	fireEvent,
+	screen,
+	waitFor,
+	waitForElementToBeRemoved,
+	within
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import forEach from 'lodash/forEach';
 import map from 'lodash/map';
 import { graphql } from 'msw';
@@ -169,6 +177,110 @@ describe('Upload list', () => {
 			expect(draggedNodeItem).toBeInTheDocument();
 			expect(draggedNodeItem).not.toHaveAttribute('disabled', '');
 			expect(screen.queryByTestId('dropzone-overlay')).not.toBeInTheDocument();
+		});
+
+		test('Drop of mixed files and folder in the upload list shows folder item as failed and a snackbar to inform upload of folder is not allowed', async () => {
+			const localRoot = populateFolder(0, ROOTS.LOCAL_ROOT);
+			const uploadedFiles = populateNodes(2, 'File') as FilesFile[];
+			const files: File[] = [];
+			// invalid folder (file without size nor type)
+			uploadedFiles[0].parent = localRoot;
+			files.push(new File([], uploadedFiles[0].name, { type: '' }));
+			// valid file
+			uploadedFiles[1].parent = localRoot;
+			files.push(new File(['(⌐□_□)'], uploadedFiles[1].name, { type: uploadedFiles[1].mime_type }));
+
+			let reqIndex = 0;
+
+			// write local root data in cache as if it was already loaded
+			const getChildrenMockedQuery = mockGetChildren(getChildrenVariables(localRoot.id), localRoot);
+			global.apolloClient.cache.writeQuery<GetChildrenQuery, GetChildrenQueryVariables>({
+				...getChildrenMockedQuery.request,
+				data: {
+					getNode: localRoot
+				}
+			});
+
+			server.use(
+				graphql.query<GetChildQuery, GetChildQueryVariables>('getChild', (req, res, ctx) => {
+					const { node_id: id } = req.variables;
+					const result = (reqIndex < uploadedFiles.length && uploadedFiles[reqIndex]) || null;
+					if (result) {
+						result.id = id;
+					}
+					reqIndex += 1;
+					return res(ctx.data({ getNode: result }));
+				})
+			);
+
+			const dataTransferObj = {
+				types: ['Files'],
+				files
+			};
+
+			const mocks = [mockGetBaseNode({ node_id: localRoot.id }, localRoot)];
+
+			render(<UploadList />, { mocks });
+
+			await screen.findByText(/nothing here/gi);
+
+			fireEvent.dragEnter(screen.getByText(/nothing here/gi), {
+				dataTransfer: dataTransferObj
+			});
+
+			await screen.findByTestId('dropzone-overlay');
+			expect(
+				screen.getByText(/Drop here your attachments to quick-add them to your Home/gm)
+			).toBeVisible();
+
+			fireEvent.drop(screen.getByText(/nothing here/gi), {
+				dataTransfer: dataTransferObj
+			});
+
+			// snackbar with action to open additional info modal is shown
+			await screen.findByText(uploadedFiles[0].name);
+			const snackbar = await screen.findByText(/some items have not been uploaded/i);
+			expect(screen.getByRole('button', { name: /more info/i })).toBeInTheDocument();
+			userEvent.click(screen.getByRole('button', { name: /more info/i }));
+			await screen.findByText(/additional info/i);
+			await waitForElementToBeRemoved(snackbar);
+
+			expect(screen.getByText(/additional info/i)).toBeInTheDocument();
+			expect(screen.getByText(/Folders cannot be uploaded in Files/i)).toBeInTheDocument();
+			expect(screen.getByTestId('icon: Close')).toBeInTheDocument();
+			userEvent.click(screen.getByTestId('icon: Close'));
+			expect(screen.queryByText(/additional info/i)).not.toBeInTheDocument();
+
+			expect(screen.getAllByTestId('node-item', { exact: false })).toHaveLength(
+				uploadedFiles.length
+			);
+			expect(screen.queryByText(/Drop here your attachments/gm)).not.toBeInTheDocument();
+
+			await waitFor(() => {
+				const localRootCachedData = global.apolloClient.readQuery<
+					GetChildrenQuery,
+					GetChildrenQueryVariables
+				>(getChildrenMockedQuery.request);
+				return expect(
+					(localRootCachedData?.getNode as Maybe<Folder> | undefined)?.children || []
+				).toHaveLength(1);
+			});
+
+			expect(screen.getByText(uploadedFiles[0].name)).toBeVisible();
+			expect(screen.getByTestId('icon: AlertCircle')).toBeVisible();
+			const folderItem = screen.getByTestId('node-item-0');
+			expect(within(folderItem).getByTestId('icon: PlayCircleOutline')).not.toHaveAttribute(
+				'disabled',
+				''
+			);
+			act(() => {
+				userEvent.click(within(folderItem).getByTestId('icon: PlayCircleOutline'));
+			});
+			const snackbar2 = await screen.findByText(/folders can not be uploaded/i);
+			expect(screen.getByRole('button', { name: /more info/i })).toBeInTheDocument();
+			await waitForElementToBeRemoved(snackbar2);
+			expect(screen.getByText(uploadedFiles[1].name)).toBeVisible();
+			expect(screen.getByTestId('icon: CheckmarkCircle2')).toBeVisible();
 		});
 	});
 });
