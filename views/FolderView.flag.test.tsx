5ecff40ef7efbf0ae0d303c116694c1b267748ec
Cataldo Mazzilli <cm@zextras.com>
2022-05-20 10:21:21 +0200

    refactor: split contextual menu, flag, getChildren, selectionMode and sorting tests

---
+++ views/FolderView.flag.test.tsx
@@ -0,0 +1,163 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import React from 'react';
+
+import { fireEvent, screen, waitForElementToBeRemoved, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import forEach from 'lodash/forEach';
+import map from 'lodash/map';
+
+import { CreateOptionsContent } from '../../hooks/useCreateOptions';
+import { populateFolder, populateNode } from '../mocks/mockUtils';
+import { Node } from '../types/common';
+import { Folder } from '../types/graphql/types';
+import { getChildrenVariables, mockFlagNodes, mockGetChildren } from '../utils/mockUtils';
+import { actionRegexp, render, selectNodes, waitForNetworkResponse } from '../utils/testUtils';
+import { DisplayerProps } from './components/Displayer';
+import FolderView from './FolderView';
+
+jest.mock('../../hooks/useCreateOptions', () => ({
+	useCreateOptions: (): CreateOptionsContent => ({
+		setCreateOptions: jest.fn(),
+		removeCreateOptions: jest.fn()
+	})
+}));
+
+jest.mock('./components/Displayer', () => ({
+	Displayer: (props: DisplayerProps): JSX.Element => (
+		<div data-testid="displayer-test-id">
+			{props.translationKey}:{props.icons}
+		</div>
+	)
+}));
+
+describe('Flag', () => {
+	describe('Selection mode', () => {
+		test('Flag/Unflag action marks all and only selected items as flagged/unflagged', async () => {
+			const currentFolder = populateFolder(4);
+			forEach(currentFolder.children, (mockedNode) => {
+				(mockedNode as Node).flagged = false;
+			});
+
+			const nodesIdsToFlag = map(
+				currentFolder.children.slice(0, currentFolder.children.length / 2),
+				(child) => (child as Node).id
+			);
+
+			const nodesIdsToUnflag = nodesIdsToFlag.slice(0, nodesIdsToFlag.length / 2);
+
+			const mocks = [
+				mockGetChildren(getChildrenVariables(currentFolder.id), {
+					...currentFolder,
+					children: currentFolder.children
+				} as Folder),
+				mockFlagNodes(
+					{
+						node_ids: nodesIdsToFlag,
+						flag: true
+					},
+					nodesIdsToFlag
+				),
+				mockFlagNodes(
+					{
+						node_ids: nodesIdsToUnflag,
+						flag: false
+					},
+					nodesIdsToUnflag
+				)
+			];
+
+			render(<FolderView />, { initialRouterEntries: [`/?folder=${currentFolder.id}`], mocks });
+
+			// wait for the load to be completed
+			await waitForElementToBeRemoved(screen.queryByTestId('icon: Refresh'));
+			expect(screen.queryByTestId('icon: Flag')).not.toBeInTheDocument();
+
+			// activate selection mode by selecting items
+			selectNodes(nodesIdsToFlag);
+
+			// check that all wanted items are selected
+			expect(screen.getAllByTestId('checkedAvatar')).toHaveLength(nodesIdsToFlag.length);
+			expect(screen.getByTestId('icon: MoreVertical')).toBeVisible();
+			userEvent.click(screen.getByTestId('icon: MoreVertical'));
+			await screen.findByText(actionRegexp.flag);
+			// click on flag action on header bar
+			userEvent.click(screen.getByText(actionRegexp.flag));
+			await waitForElementToBeRemoved(screen.queryAllByTestId('checkedAvatar'));
+			await screen.findAllByTestId('icon: Flag');
+			expect(screen.getAllByTestId('icon: Flag')).toHaveLength(nodesIdsToFlag.length);
+
+			// activate selection mode by selecting items
+			selectNodes(nodesIdsToUnflag);
+			// check that all wanted items are selected
+			expect(screen.getAllByTestId('checkedAvatar')).toHaveLength(nodesIdsToUnflag.length);
+			expect(screen.getByTestId('icon: MoreVertical')).toBeVisible();
+			userEvent.click(screen.getByTestId('icon: MoreVertical'));
+			await screen.findByText(actionRegexp.unflag);
+			// click on unflag action on header bar
+			userEvent.click(screen.getByText(actionRegexp.unflag));
+			await waitForElementToBeRemoved(screen.queryAllByTestId('checkedAvatar'));
+			await screen.findAllByTestId('icon: Flag');
+			expect(screen.getAllByTestId('icon: Flag')).toHaveLength(
+				nodesIdsToFlag.length - nodesIdsToUnflag.length
+			);
+		});
+	});
+
+	describe('Contextual menu actions', () => {
+		test('click on flag action changes flag icon visibility', async () => {
+			const currentFolder = populateFolder();
+			const node = populateNode();
+			// set the node not flagged so that we can search by flag action in the contextual menu of first node
+			node.flagged = false;
+			currentFolder.children.push(node);
+
+			const mocks = [
+				mockGetChildren(getChildrenVariables(currentFolder.id), currentFolder),
+				mockFlagNodes(
+					{
+						node_ids: [node.id],
+						flag: true
+					},
+					[node.id]
+				),
+				mockFlagNodes(
+					{
+						node_ids: [node.id],
+						flag: false
+					},
+					[node.id]
+				)
+			];
+
+			render(<FolderView />, { initialRouterEntries: [`/?folder=${currentFolder.id}`], mocks });
+
+			// wait for the load to be completed
+			await waitForElementToBeRemoved(screen.queryByTestId('icon: Refresh'));
+
+			// right click to open contextual menu
+			const nodeItem = screen.getByTestId(`node-item-${node.id}`);
+			// open context menu and click on flag action
+			fireEvent.contextMenu(nodeItem);
+			const flagAction = await screen.findByText(actionRegexp.flag);
+			expect(flagAction).toBeVisible();
+			userEvent.click(flagAction);
+			await waitForNetworkResponse();
+			await within(nodeItem).findByTestId('icon: Flag');
+			expect(flagAction).not.toBeInTheDocument();
+			expect(within(nodeItem).getByTestId('icon: Flag')).toBeVisible();
+			// open context menu and click on unflag action
+			fireEvent.contextMenu(nodeItem);
+			const unflagAction = await screen.findByText(actionRegexp.unflag);
+			expect(unflagAction).toBeVisible();
+			userEvent.click(unflagAction);
+			await waitForNetworkResponse();
+			expect(unflagAction).not.toBeInTheDocument();
+			expect(within(nodeItem).queryByTestId('icon: Flag')).not.toBeInTheDocument();
+		});
+	});
+});
---
+++ views/FolderView.selectionMode.test.tsx
@@ -0,0 +1,139 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import forEach from 'lodash/forEach';
+
+import { CreateOptionsContent } from '../../hooks/useCreateOptions';
+import { NODES_LOAD_LIMIT } from '../constants';
+import { populateFolder, populateNodes } from '../mocks/mockUtils';
+import { Node } from '../types/common';
+import { Folder } from '../types/graphql/types';
+import { getChildrenVariables, mockGetChildren, mockGetParent } from '../utils/mockUtils';
+import { actionRegexp, render, selectNodes, triggerLoadMore } from '../utils/testUtils';
+import { DisplayerProps } from './components/Displayer';
+import FolderView from './FolderView';
+
+jest.mock('../../hooks/useCreateOptions', () => ({
+	useCreateOptions: (): CreateOptionsContent => ({
+		setCreateOptions: jest.fn(),
+		removeCreateOptions: jest.fn()
+	})
+}));
+
+jest.mock('./components/Displayer', () => ({
+	Displayer: (props: DisplayerProps): JSX.Element => (
+		<div data-testid="displayer-test-id">
+			{props.translationKey}:{props.icons}
+		</div>
+	)
+}));
+
+describe('Selection mode', () => {
+	test('if there is no element selected, all actions are visible and disabled', async () => {
+		const currentFolder = populateFolder(10);
+		const mocks = [
+			mockGetParent({ node_id: currentFolder.id }, currentFolder),
+			mockGetChildren(getChildrenVariables(currentFolder.id), currentFolder)
+		];
+		render(<FolderView />, { initialRouterEntries: [`/?folder=${currentFolder.id}`], mocks });
+
+		await screen.findByText((currentFolder.children[0] as Node).name);
+		expect(screen.getByText((currentFolder.children[0] as Node).name)).toBeVisible();
+		expect(screen.queryByTestId('checkedAvatar')).not.toBeInTheDocument();
+		selectNodes([(currentFolder.children[0] as Node).id]);
+		// check that all wanted items are selected
+		expect(screen.getByTestId('checkedAvatar')).toBeInTheDocument();
+		expect(screen.getByText(/select all/i)).toBeVisible();
+		// deselect node. Selection mode remains active
+		selectNodes([(currentFolder.children[0] as Node).id]);
+		expect(screen.queryByTestId('checkedAvatar')).not.toBeInTheDocument();
+		expect(screen.getAllByTestId('unCheckedAvatar')).toHaveLength(currentFolder.children.length);
+		expect(screen.getByText(/select all/i)).toBeVisible();
+		expect(screen.getByTestId('icon: Trash2Outline')).toBeVisible();
+		expect(screen.getByTestId('icon: Trash2Outline').parentNode).toHaveAttribute('disabled', '');
+		expect(screen.getByTestId('icon: MoreVertical')).toBeVisible();
+		userEvent.click(screen.getByTestId('icon: MoreVertical'));
+		await screen.findByText(actionRegexp.rename);
+		expect(screen.getByText(actionRegexp.rename)).toBeVisible();
+		expect(screen.getByText(actionRegexp.rename).parentNode).toHaveAttribute('disabled', '');
+		expect(screen.getByText(actionRegexp.copy)).toBeVisible();
+		expect(screen.getByText(actionRegexp.copy).parentNode).toHaveAttribute('disabled', '');
+		expect(screen.getByText(actionRegexp.move)).toBeVisible();
+		expect(screen.getByText(actionRegexp.move).parentNode).toHaveAttribute('disabled', '');
+		expect(screen.getByText(actionRegexp.flag)).toBeVisible();
+		expect(screen.getByText(actionRegexp.flag).parentNode).toHaveAttribute('disabled', '');
+		expect(screen.getByText(actionRegexp.unflag)).toBeVisible();
+		expect(screen.getByText(actionRegexp.unflag).parentNode).toHaveAttribute('disabled', '');
+		expect(screen.getByText(actionRegexp.download)).toBeVisible();
+		expect(screen.getByText(actionRegexp.download).parentNode).toHaveAttribute('disabled', '');
+		expect(screen.getByText(actionRegexp.openDocument)).toBeVisible();
+		expect(screen.getByText(actionRegexp.openDocument).parentNode).toHaveAttribute('disabled', '');
+		expect(screen.queryByTestId('icon: RestoreOutline')).not.toBeInTheDocument();
+		expect(screen.queryByTestId('icon: DeletePermanentlyOutline')).not.toBeInTheDocument();
+		expect(screen.getByTestId('icon: ArrowBackOutline')).toBeVisible();
+		userEvent.click(screen.getByTestId('icon: ArrowBackOutline'));
+		expect(screen.queryByTestId('icon: Trash2Outline')).not.toBeInTheDocument();
+		expect(screen.queryByTestId('unCheckedAvatar')).not.toBeInTheDocument();
+		expect(screen.queryByTestId('checkedAvatar')).not.toBeInTheDocument();
+		expect(screen.queryByText(/select all/i)).not.toBeInTheDocument();
+	});
+
+	test('if all loaded nodes are selected, unselect all action is visible', async () => {
+		const currentFolder = populateFolder(NODES_LOAD_LIMIT);
+		const secondPage = populateNodes(10) as Node[];
+		forEach(secondPage, (mockedNode) => {
+			mockedNode.parent = currentFolder;
+		});
+		const mocks = [
+			mockGetParent({ node_id: currentFolder.id }, currentFolder),
+			mockGetChildren(getChildrenVariables(currentFolder.id), currentFolder),
+			mockGetChildren(
+				{
+					...getChildrenVariables(currentFolder.id),
+					cursor: (currentFolder.children[NODES_LOAD_LIMIT - 1] as Node).id
+				},
+				{ ...currentFolder, children: secondPage } as Folder
+			)
+		];
+		render(<FolderView />, { initialRouterEntries: [`/?folder=${currentFolder.id}`], mocks });
+
+		await screen.findByText((currentFolder.children[0] as Folder).name);
+		expect(screen.queryByText(/select all/i)).not.toBeInTheDocument();
+		selectNodes([(currentFolder.children[0] as Folder).id]);
+		// check that all wanted items are selected
+		expect(screen.getByTestId('checkedAvatar')).toBeInTheDocument();
+		expect(screen.getByText(/\bselect all/i)).toBeVisible();
+		userEvent.click(screen.getByText(/\bselect all/i));
+		await screen.findByText(/deselect all/i);
+		expect(screen.queryByTestId('unCheckedAvatar')).not.toBeInTheDocument();
+		expect(screen.getAllByTestId('checkedAvatar')).toHaveLength(currentFolder.children.length);
+		expect(screen.getByText(/deselect all/i)).toBeVisible();
+		expect(screen.queryByText(/\bselect all/i)).not.toBeInTheDocument();
+		await triggerLoadMore();
+		await screen.findByText(secondPage[0].name);
+		expect(screen.getAllByTestId('checkedAvatar')).toHaveLength(currentFolder.children.length);
+		expect(screen.getAllByTestId('unCheckedAvatar')).toHaveLength(secondPage.length);
+		expect(screen.queryByText(/deselect all/i)).not.toBeInTheDocument();
+		expect(screen.getByText(/\bselect all/i)).toBeVisible();
+		userEvent.click(screen.getByText(/\bselect all/i));
+		await screen.findByText(/deselect all/i);
+		expect(screen.getAllByTestId('checkedAvatar')).toHaveLength(
+			currentFolder.children.length + secondPage.length
+		);
+		expect(screen.queryByTestId('unCheckedAvatar')).not.toBeInTheDocument();
+		expect(screen.getByText(/deselect all/i)).toBeVisible();
+		userEvent.click(screen.getByText(/deselect all/i));
+		await screen.findByText(/\bselect all/i);
+		expect(screen.getAllByTestId('unCheckedAvatar')).toHaveLength(
+			currentFolder.children.length + secondPage.length
+		);
+		expect(screen.queryByTestId('checkedAvatar')).not.toBeInTheDocument();
+	});
+});
