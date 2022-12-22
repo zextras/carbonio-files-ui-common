63e44c113aff5833c51f7f995a6c4a7b713657d5
Beatrice Guerra <beatrice.guerra@zextras.com>
2022-12-22 14:36:44 +0100

    test: add test for go to folder on upload list

--- views/components/UploadList.goToFolder.test.tsx
+++ views/components/UploadList.goToFolder.test.tsx
@@ -3,30 +3,79 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+import keyBy from 'lodash/keyBy';
+
+import { uploadVar } from '../../apollo/uploadVar';
+import { ICON_REGEXP } from '../../constants/test';
+import { populateFolder, populateLocalRoot, populateUploadItems } from '../../mocks/mockUtils';
+import { UploadStatus } from '../../types/common';
+import { mockGetBaseNode } from '../../utils/mockUtils';
+import { selectNodes, setup } from '../../utils/testUtils';
+import { UploadList } from './UploadList';
+
 describe('Upload List', () => {
 	describe('Go To Folder', () => {
 		describe('Selection Mode', () => {
-			test.todo('Action is visible even if selected items have all a different status');
-		});
+			test('Action is visible if selected items have all the same parent, independently from the status', async () => {
+				const localRoot = populateLocalRoot();
+				const statuses = Object.values(UploadStatus);
+				const uploadItems = populateUploadItems(statuses.length);
+				statuses.forEach((status, index) => {
+					uploadItems[index].status = status;
+					uploadItems[index].parentNodeId = localRoot.id;
+				});
 
-		describe('Contextual Menu', () => {
-			test.todo('Action is visible if item is failed');
+				const uploadMap = keyBy(uploadItems, (item) => item.id);
 
-			test.todo('Action is visible if item is loading');
+				uploadVar(uploadMap);
 
-			test.todo('Action is visible if item is queued');
+				const mocks = [mockGetBaseNode({ node_id: localRoot.id }, localRoot)];
 
-			test.todo('Action is visible if item is completed');
-		});
+				const { user, getByRoleWithIcon } = setup(<UploadList />, { mocks });
 
-		describe('Hover bar', () => {
-			test.todo('Action is visible if item is failed');
+				await screen.findByText(uploadItems[0].name);
 
-			test.todo('Action is visible if item is loading');
+				await selectNodes(Object.keys(uploadMap), user);
 
-			test.todo('Action is visible if item is queued');
+				await screen.findByText(/deselect all/i);
+				expect(getByRoleWithIcon('button', { icon: ICON_REGEXP.goToFolder })).toBeVisible();
+				expect(getByRoleWithIcon('button', { icon: ICON_REGEXP.goToFolder })).toBeEnabled();
+			});
 
-			test.todo('Action is visible if item is completed');
+			test('Action is hidden if selected at least one of the selected items has a different parent', async () => {
+				const localRoot = populateLocalRoot();
+				const statuses = Object.values(UploadStatus);
+				const uploadItems = populateUploadItems(statuses.length);
+				statuses.forEach((status, index) => {
+					uploadItems[index].status = status;
+					uploadItems[index].parentNodeId = localRoot.id;
+				});
+				const differentParent = populateFolder();
+				uploadItems[uploadItems.length - 1].parentNodeId = differentParent.id;
+
+				const uploadMap = keyBy(uploadItems, (item) => item.id);
+
+				uploadVar(uploadMap);
+
+				const mocks = [
+					mockGetBaseNode({ node_id: localRoot.id }, localRoot),
+					mockGetBaseNode({ node_id: differentParent.id }, differentParent)
+				];
+
+				const { user, queryByRoleWithIcon } = setup(<UploadList />, { mocks });
+
+				await screen.findByText(uploadItems[0].name);
+
+				await selectNodes(Object.keys(uploadMap), user);
+
+				await screen.findByText(/deselect all/i);
+				expect(
+					queryByRoleWithIcon('button', { icon: ICON_REGEXP.goToFolder })
+				).not.toBeInTheDocument();
+			});
 		});
 	});
 });
